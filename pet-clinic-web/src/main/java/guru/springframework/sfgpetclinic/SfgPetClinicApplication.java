fd3e28b4265e8737ec58e7a53a7054e3f85eb628
Marcello <mardigraweb@gmail.com>
2020-12-31 18:41:21 +0100

    Still trying to fix issue n.3 about maven release plugin

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -3,6 +3,9 @@ package guru.springframework.sfgpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Add a basic comment to check how the release plugin update github.com
+ */
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
